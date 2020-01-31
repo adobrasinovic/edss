77d7c654f235f7e61ebcedd71d2ee697e64aded6
Aleksandar Dobrasinovic <adobrasinovic@gmail.com>
2020-01-31 13:23:13 +0100

    - Added tests for argument bounds - Changed argument type to check if it is a positive integer to simplify bounds check - Added bounds checks to index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
-// VISUAL (OPTIC) FUNCTIONAL SYSTEM SCORE
-// 0 - normal.
-// 1 - disc pallor and / or small scotoma and / or visual acuity (corrected) of worse eye less than 20 / 20 (1.0) but better than 20 / 30 (0.67).
-// 2 - worse eye with maximal visual acuity (corrected) of 20 / 30 to 20 / 59 (0.67-0.34).
-// 3 - worse eye with large scotoma and/or moderate decrease in fields and/or maximal visual acuity (corrected) of 20 / 60 to 20 / 99 (0.33-0.21).
-// 4 - worse eye with marked decrease of fields and/or maximal visual acuity (corrected) of 20 / 100 to 20 / 200 (0.2-0.1); grade 3 plus maximal acuity of better eye of 20 / 60 (0.33) or less.
-// 5 - worse eye with maximal visual acuity (corrected) less than 20 / 200 (0.1); grade 4 plus maximal acuity of better eye of 20 / 60 (0.33) or less.
-// 6 - grade 5 plus maximal visual acuity of better eye of 20 / 60 (0.33) or less.
-
 function calculateEDSS(visualFunctionsScore, brainstemFunctionScore, pyramidalFunctionsScore, cerebellarFunctionsScore,
                        sensoryFunctionsScore, bowelAndBladderFunctionsScore, cerebralFunctionsScore, ambulationScore) {
 
     if (arguments.length !== 8) {
-       throw new Error(`Wrong number of parameters. arguments.length = ${arguments.length}`); 
+       throw new Error(`Wrong number of arguments. arguments.length = ${arguments.length}`); 
     }
 
     const argumentsArray = Array.from(arguments);
 
     for (let argument of argumentsArray) {
-        if (argument == null  || isNaN(argument)) {
-            throw new TypeError(`Argument is not a number. argument = ${argument}`);
+        if (argument == null  || !Number.isInteger(+argument) || +argument < 0) {
+            throw new TypeError(`Argument is not a positive integer. argument = ${argument}`);
         }
     }
 
-    return (+visualFunctionsScore) + (+brainstemFunctionScore + (+pyramidalFunctionsScore) + (+cerebellarFunctionsScore) + (+sensoryFunctionsScore) + 
-        (+bowelAndBladderFunctionsScore) + (+cerebralFunctionsScore) + (+ambulationScore));
+    visualFunctionsScore = +visualFunctionsScore;
+
+    if (visualFunctionsScore > 6) {
+        throw new Error(`Argument is out of bounds. visualFunctionsScore`); 
+    }
+
+    brainstemFunctionScore = +brainstemFunctionScore;
+
+    if (brainstemFunctionScore > 5) {
+        throw new Error(`Argument is out of bounds. brainstemFunctionScore`); 
+    }
+
+    pyramidalFunctionsScore = +pyramidalFunctionsScore;
+
+    if (pyramidalFunctionsScore > 6) {
+        throw new Error(`Argument is out of bounds. pyramidalFunctionsScore`); 
+    }
+
+    cerebellarFunctionsScore = +cerebellarFunctionsScore;
+
+    if (cerebellarFunctionsScore > 5) {
+        throw new Error(`Argument is out of bounds. cerebellarFunctionsScore`); 
+    }
+
+    sensoryFunctionsScore = +sensoryFunctionsScore;
+
+    if (sensoryFunctionsScore > 6) {
+        throw new Error(`Argument is out of bounds. sensoryFunctionsScore`); 
+    }
+
+    bowelAndBladderFunctionsScore = +bowelAndBladderFunctionsScore;
+
+    if (bowelAndBladderFunctionsScore > 6) {
+        throw new Error(`Argument is out of bounds. bowelAndBladderFunctionsScore`); 
+    }
+
+    cerebralFunctionsScore = +cerebralFunctionsScore;
+
+    if (cerebralFunctionsScore > 5) {
+        throw new Error(`Argument is out of bounds. cerebralFunctionsScore`); 
+    }
+
+    ambulationScore = +ambulationScore;
+
+    if (ambulationScore > 15) {
+        throw new Error(`Argument is out of bounds. ambulationScore`); 
+    }
+
+    return 0;
 }
 
 module.exports = {
--- src/index.test.js
+++ src/index.test.js
@@ -4,16 +4,38 @@ var edss = require('./index');
 
 describe("calculateEDSS", function(){
 
-    it("should throw an error if there is a wrong number of parameters", function(){
-        expect(() => edss.calculateEDSS()).to.throw('Wrong number of parameters.'); 
-        expect(() => edss.calculateEDSS(1,1,1)).to.throw('Wrong number of parameters.'); 
-        expect(() => edss.calculateEDSS(1,1,1,1,1,1,1,1,1,1,1)).to.throw('Wrong number of parameters.');
+    it("should throw an error if there is a wrong number of arguments", function(){
+        expect(() => edss.calculateEDSS()).to.throw('Wrong number of arguments.'); 
+        expect(() => edss.calculateEDSS(1,1,1)).to.throw('Wrong number of arguments.'); 
+        expect(() => edss.calculateEDSS(1,1,1,1,1,1,1,1,1,1,1)).to.throw('Wrong number of arguments.');
     });
 
-    it("should throw an error if there is a parameter that is not a number (checking for null,undefined and isNaN)", function(){
-        expect(() => edss.calculateEDSS(1,1,1,1,'test',1,1,1)).to.throw(TypeError, 'Argument is not a number.'); 
-        expect(() => edss.calculateEDSS(null,1,1,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a number.'); 
-        expect(() => edss.calculateEDSS(undefined,1,1,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a number.'); 
+    it("should throw an error if there is an argument that is not a positive integer (checking for null,undefined and if argument is an integer)", function(){
+        expect(() => edss.calculateEDSS(1,1,1,1,'test',1,1,1)).to.throw(TypeError, 'Argument is not a positive integer.'); 
+        expect(() => edss.calculateEDSS(1,1,1.23,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a positive integer.'); 
+        expect(() => edss.calculateEDSS(-1,1,1,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a positive integer.'); 
+        expect(() => edss.calculateEDSS(null,1,1,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a positive integer.'); 
+        expect(() => edss.calculateEDSS(undefined,1,1,1,1,1,1,1)).to.throw(TypeError, 'Argument is not a positive integer.'); 
+    });
+
+    // visualFunctionsScore 0 - 6
+    // brainstemFunctionScore 0 - 5
+    // pyramidalFunctionsScore 0 - 6 
+    // cerebellarFunctionsScore 0 - 5
+    // sensoryFunctionsScore 0 - 6
+    // bowelAndBladderFunctionsScore 0 - 6
+    // cerebralFunctionsScore 0 - 5
+    // ambulationScore 0 - 15
+
+    it("should throw an error if an argument is out of bounds", function(){
+        expect(() => edss.calculateEDSS(7,1,1,1,1,1,1,1)).to.throw('Argument is out of bounds. visualFunctionsScore'); 
+        expect(() => edss.calculateEDSS(1,6,1,1,1,1,1,1)).to.throw('Argument is out of bounds. brainstemFunctionScore'); 
+        expect(() => edss.calculateEDSS(1,1,7,1,1,1,1,1)).to.throw('Argument is out of bounds. pyramidalFunctionsScore'); 
+        expect(() => edss.calculateEDSS(1,1,1,6,1,1,1,1)).to.throw('Argument is out of bounds. cerebellarFunctionsScore'); 
+        expect(() => edss.calculateEDSS(1,1,1,1,7,1,1,1)).to.throw('Argument is out of bounds. sensoryFunctionsScore');
+        expect(() => edss.calculateEDSS(1,1,1,1,1,7,1,1)).to.throw('Argument is out of bounds. bowelAndBladderFunctionsScore'); 
+        expect(() => edss.calculateEDSS(1,1,1,1,1,1,6,1)).to.throw('Argument is out of bounds. cerebralFunctionsScore'); 
+        expect(() => edss.calculateEDSS(1,1,1,1,1,1,1,16)).to.throw('Argument is out of bounds. ambulationScore'); 
     });
 
 });
